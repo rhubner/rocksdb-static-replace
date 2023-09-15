e856c7c1069a3c489e97bd5074c1de88dcaabe05
Radek Hubner <rhubner@users.noreply.github.com>
2023-09-15 13:18:01 +0100

    ColumngFamilyOptions

---
+++ ColumnFamily.sed
@@ -0,0 +1,5 @@
+3930,5819s/(JNIEnv\* \w+), jobject,/\1, jclass,/
+3930,5819s/(JNIEnv\* \w+), jobject /\1, jclass /
+3930,5819s/\JNIEnv\*, jobject,/\JNIEnv\*, jclass,/
+3930,5819s/      jobject,/      jclass,/
+3930,5819s/disposeInternal\(JNIEnv\*, jclass,/disposeInternal\(JNIEnv\*, jobject,/
--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+REPLACE_FILE=options.cc
+
 cd ../rocksdb
-git restore java/rocksjni/rocksjni.cc
-gsed -i -r -f ../rocksdb-static-replace/replace.sed java/rocksjni/rocksjni.cc
-git diff java/rocksjni/rocksjni.cc
+git restore java/rocksjni/$REPLACE_FILE
+gsed -i -r -f ../rocksdb-static-replace/ColumnFamily.sed java/rocksjni/$REPLACE_FILE
+git diff java/rocksjni/$REPLACE_FILE
 
 #s/(JNIEnv\* \w+), jobject /\1, jclass /
